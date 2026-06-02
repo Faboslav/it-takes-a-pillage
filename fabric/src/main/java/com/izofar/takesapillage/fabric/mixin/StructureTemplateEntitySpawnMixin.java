10c1fac3fdf81271879dbbe6207ad6b2362ff597
Faboslav <fabianek.ales@gmail.com>
2026-06-02 16:51:15 +0200

    Fixed mixin

--- fabric/src/main/java/com/izofar/takesapillage/fabric/mixin/StructureTemplateEntitySpawnMixin.java
+++ fabric/src/main/java/com/izofar/takesapillage/fabric/mixin/StructureTemplateEntitySpawnMixin.java
@@ -1,12 +1,9 @@
 package com.izofar.takesapillage.fabric.mixin;
 
 import com.izofar.takesapillage.common.event.entity.EntitySpawnEvent;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.minecraft.world.DifficultyInstance;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.SpawnGroupData;
 import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.block.Mirror;
 import net.minecraft.world.level.block.Rotation;
@@ -14,63 +11,61 @@ import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemp
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-//? if >= 1.21.3 {
+/*? >=1.21.3 {*/
 import net.minecraft.world.entity.EntitySpawnReason;
-//?} else {
+/*?} else {*/
 /*import net.minecraft.world.entity.MobSpawnType;
- *///?}
+ *//*?}*/
 
 @Mixin(StructureTemplate.class)
 public class StructureTemplateEntitySpawnMixin
 {
-	@WrapOperation(
+    @Inject(
 		//? if >= 26.1 {
 		method = "lambda$placeEntities$0",
 		//?} else {
 		/*method = "method_17917",
 		*///?}
-		at = @At(
-			value = "INVOKE",
-			/*? if >=1.21.3 {*/
-			target = "Lnet/minecraft/world/entity/Mob;finalizeSpawn(Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/world/DifficultyInstance;Lnet/minecraft/world/entity/EntitySpawnReason;Lnet/minecraft/world/entity/SpawnGroupData;)Lnet/minecraft/world/entity/SpawnGroupData;"
-			/*?} else {*/
-			/*target = "Lnet/minecraft/world/entity/Mob;finalizeSpawn(Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/world/DifficultyInstance;Lnet/minecraft/world/entity/MobSpawnType;Lnet/minecraft/world/entity/SpawnGroupData;)Lnet/minecraft/world/entity/SpawnGroupData;"
-			*//*?}*/
-		)
+            at = @At(
+				value = "INVOKE",
+				//? >=1.21.3 {
+				target = "Lnet/minecraft/world/entity/Mob;finalizeSpawn(Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/world/DifficultyInstance;Lnet/minecraft/world/entity/EntitySpawnReason;Lnet/minecraft/world/entity/SpawnGroupData;)Lnet/minecraft/world/entity/SpawnGroupData;"
+				//?} else if >=1.21.1 {
+				/*target = "Lnet/minecraft/world/entity/Mob;finalizeSpawn(Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/world/DifficultyInstance;Lnet/minecraft/world/entity/MobSpawnType;Lnet/minecraft/world/entity/SpawnGroupData;)Lnet/minecraft/world/entity/SpawnGroupData;"
+				*///?} else {
+				/*target = "Lnet/minecraft/world/entity/Mob;finalizeSpawn(Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/world/DifficultyInstance;Lnet/minecraft/world/entity/MobSpawnType;Lnet/minecraft/world/entity/SpawnGroupData;Lnet/minecraft/nbt/CompoundTag;)Lnet/minecraft/world/entity/SpawnGroupData;"
+				*///?}
+			),
+            cancellable = true
 	)
-	private static SpawnGroupData takesapillage$onFinalizeSpawn(
-		Mob mob,
-		ServerLevelAccessor level,
-		DifficultyInstance difficulty,
-		/*? if >=1.21.3 {*/
-		EntitySpawnReason spawnReason,
-		/*?} else {*/
-		/*MobSpawnType spawnReason,
-		 *//*?}*/
-		SpawnGroupData spawnGroupData,
-		Operation<SpawnGroupData> original, Rotation rotation,
+    private static void takesapillage$onEntitySpawn(
+		Rotation rotation,
 		Mirror mirror,
-		Vec3 pos,
-		boolean finalizeEntities,
-		ServerLevelAccessor capturedLevel,
-		Entity entity
-	) {
-		if (!EntitySpawnEvent.EVENT.invoke(
-			new EntitySpawnEvent(
-				mob,
-				level,
-				mob.isBaby(),
-				/*? if >=1.21.3 {*/
-				EntitySpawnReason.STRUCTURE
-				/*?} else {*/
-				/*MobSpawnType.STRUCTURE
-				 *//*?}*/
-			)
-		)) {
-			return original.call(mob, level, difficulty, spawnReason, spawnGroupData);
-		} else {
-			return null;
-		}
-	}
+		Vec3 vec3,
+		boolean bl,
+		ServerLevelAccessor serverLevel,
+		/*? <1.21.1 {*/ /*CompoundTag compoundTag,*//*?}*/
+		Entity entity,
+		CallbackInfo ci)
+    {
+        if (
+			entity instanceof Mob mob
+			&& EntitySpawnEvent.EVENT.invoke(
+				new EntitySpawnEvent(
+					mob,
+					serverLevel,
+					mob.isBaby(),
+					/*? >=1.21.3 {*/
+					EntitySpawnReason.STRUCTURE
+					/*?} else {*/
+					/*MobSpawnType.STRUCTURE
+					*//*?}*/
+				)
+			)) {
+            ci.cancel();
+        }
+    }
 }
